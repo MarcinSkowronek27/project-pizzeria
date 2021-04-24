19a252b9af2e05e69d3db0ffd7a4692252d6dd43
Marcin Skowronek <marcinfoto35@op.pl>
2021-04-24 20:05:03 +0200

    Done first task

--- src/js/script.js
+++ src/js/script.js
@@ -84,10 +84,10 @@
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
-        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         if (activeProduct != null && activeProduct != thisProduct.element){
-          activeProduct.element.classList.remove(select.menuProduct.clickable);
+          activeProduct.classList.remove('active');
         }
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle('active');
