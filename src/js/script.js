43f434721686169e288a418012fdfd9066c5d9f6
Marcin Skowronek <marcinfoto35@op.pl>
2021-04-24 15:52:32 +0200

    checkpoint 5

--- src/js/script.js
+++ src/js/script.js
@@ -76,20 +76,21 @@
     initAccordion() {
       const thisProduct = this;
 
-      /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = this;
+      /* find the clickable trigger (the element that should react to clicking) */ 
+      const clickableTrigger = document.querySelector(select.menuProduct.clickable);
 
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function (event) {
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
-        const activeProduct = article.querySelectorAll(classNames.menuProduct.wrapperActive);
+        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        if (activeProduct = 'active' && activeProduct != thisProduct.element){
-          activeProduct.classList.remove('active');
+        if (activeProduct != null && activeProduct != thisProduct.element){
+          activeProduct.element.classList.remove(select.menuProduct.clickable);
         }
         /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
       })
     }
   }
