03817693854d36cbf4addd45eec6faa29e20da15
Marcin Skowronek <marcinfoto35@op.pl>
2021-04-25 10:21:08 +0200

    Fixed lack of semicolon

--- src/js/script.js
+++ src/js/script.js
@@ -91,7 +91,7 @@
         }
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
-      })
+      });
     }
   }
   const app = {
