b79648ecce0368c98702d3ff5c79286134de292d
Marcin Skowronek <marcinfoto35@op.pl>
2021-04-26 09:48:06 +0200

    problems

--- src/js/script.js
+++ src/js/script.js
@@ -141,10 +141,24 @@
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if (formData[paramId] && formData[paramId].includes(optionId)) {
+            // check if the option is not default
+            if (option.default = false) {
+              // add option price to price variable
+              price = thisProduct.params.option.price + option.price;
+            }
+          } else {
+            // check if the option is default
+            if (option.default = true) {
+              // reduce price variable
+              price = thisProduct.priceElem - option.price;
+            }
+            // update calculated price in the HTML
+            thisProduct.priceElem.innerHTML = price;
+          }
         }
       }
-      // update calculated price in the HTML
-      thisProduct.priceElem.innerHTML = price;
     }
   }
   const app = {
@@ -173,5 +187,4 @@
   };
 
   app.init();
-
 }
