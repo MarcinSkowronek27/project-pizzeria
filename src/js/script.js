8e919fab675a0fd085518a6ca893ac1f65a1f02a
Marcin Skowronek <marcinfoto35@op.pl>
2021-04-29 13:28:43 +0200

    bug 2

--- src/js/script.js
+++ src/js/script.js
@@ -180,6 +180,7 @@
             }
           }
         }
+        price = price * thisProduct.amountWidget.value;
         // update calculated price in the HTML
         thisProduct.priceElem.innerHTML = price;
       }
